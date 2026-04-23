91e8e2b97d43781de439c5040c33b7598d62cb92
Arina <arina.shushakova040408@yandex.ru>
2026-04-23 23:47:16 +0700

    Fix SQLite path for Render

--- config.py
+++ config.py
@@ -2,10 +2,13 @@ import os
 from dotenv import load_dotenv
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+os.makedirs(INSTANCE_DIR, exist_ok=True)
+
 load_dotenv(os.path.join(BASE_DIR, ".env"))
+
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
-
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "instance", "app.db")
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(INSTANCE_DIR, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
